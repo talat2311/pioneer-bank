e657fb03e0e13b2b08723187eda4d236d2b2a908
talat2311 <talatmcc@gmail.com>
2022-07-30 13:41:42 +0600

    added all

--- script.js
+++ script.js
@@ -35,6 +35,10 @@ depositBtn.addEventListener("click" , function(){
 const withdrawBtn = document.getElementById("addWithdraw");
 withdrawBtn.addEventListener("click" , function(){
     const withdrawNumber = getInputNumber("withdrawAmount");
+
+    updateSpanText("currentWithdraw" , withdrawNumber);
+    updateSpanText("currentBalance" , -1*withdrawNumber);
+    document.getElementById("withdrawAmount").value = "" ;
 })
 
 function getInputNumber(id){
